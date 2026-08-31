8f8ecbc2dcb119694d4a99840cfb12359cfc7dac
Diego Chan <d.chan@masmedan.com>
2026-08-31 16:13:59 -0500

    feat: add capitalize utility function and update transfers component to use it for status labels

--- src/app/pages/transfers/transfers.ts
+++ src/app/pages/transfers/transfers.ts
@@ -1,9 +1,11 @@
 import { httpResource } from "@angular/common/http";
-import { Component } from "@angular/core";
+import { Component, signal } from "@angular/core";
 import { TransferCard } from "@components/transfer-card/transfer-card";
 import { APIResponse } from "@core/classes/api-response";
+import { JobStatus } from "@core/enums/job-status.enum";
 import { ListResponse } from "@core/interfaces/list-response";
 import { IJobSummary, JobSummary } from "@core/models/job-summary";
+import { capitalize } from "@core/utils";
 import { environment } from "@env/environment";
 import { LucideFaceSlightlyFrowning, LucideLoaderCircle } from "@lucide/angular";
 
@@ -13,8 +15,20 @@ import { LucideFaceSlightlyFrowning, LucideLoaderCircle } from "@lucide/angular"
     templateUrl: "./transfers.html",
 })
 export class Transfers {
-    transfers = httpResource<JobSummary[]>(() => `${environment.apiUrl}/api/v1/transfers/`, {
+    public status = signal<string>("All");
+
+    transfers = httpResource<JobSummary[]>(() => {
+        const status = this.status();
+        const url = `${environment.apiUrl}/api/v1/transfers/`;
+        return status === "All" ? url : `${url}?status=${status}`;
+    }, {
         parse: (body: unknown) => APIResponse.listUnpack(JobSummary, body as ListResponse<IJobSummary>),
         defaultValue: []
     });
+
+    public jobStatusMembers = Object.entries(JobStatus);
+
+    public capitalize(text: string): string {
+        return capitalize(text);
+    }
 }
